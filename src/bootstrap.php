f2eefaaba47205816834c3078e74b9718960e022
t0xicCode <xavier.l@t0xic.co.de>
2015-04-19 12:22:41 -0400

    Moved boostrap and routes to separate files

---
+++ src/bootstrap.php
@@ -0,0 +1,37 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$config = require __DIR__ . '/../config.php';
+
+$app = new \Slim\Slim(
+    array(
+        'view' => new \Slim\Views\Twig(),
+        'debug' => false,
+    ) + $config['slim']
+);
+
+$app->configureMode(
+    'development',
+    function () use ($app) {
+        $app->config('debug', true);
+    }
+);
+
+$app->add(new \phpLDAPpass\Slim\Middleware\CsrfMiddleware());
+$app->add(new \phpLDAPpass\Slim\Middleware\SessionMiddleware());
+
+/** @var \Slim\Views\Twig $view */
+$view = $app->view();
+
+$view->parserOptions = $config['twig']['environment'];
+
+$view->parserExtensions = array(
+    new \Slim\Views\TwigExtension(),
+);
+
+$view->getEnvironment()->addGlobal('site', $config['site']);
+
+require __DIR__ . '/routes.php';
+
+return $app;
